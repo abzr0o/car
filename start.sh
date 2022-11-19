d8aa3417a823b409bd3ccc6b9315492b79fbd300
abzro0o <adqe98877@gmail.com>
2022-11-19 20:55:33 +0300

    v1

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-sudo apt install chromium-browser
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs
 sudo npm install
 
 sudo npm i -g typescript
 
 sudo tsc 
+sudo apt install chromium-browser
 
 node src/index.js
